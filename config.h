db62d25702cb8b107af2facc6e96284f40364d49
Fernando <fherpato8@gmail.com>
2021-08-31 10:49:07 -0600

    Aded singnal updates every time I change kbrlayouts

--- config.h
+++ config.h
@@ -87,8 +87,8 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_e,      spawn,          SHCMD("setxkbmap us") },
- 	{ MODKEY,                       XK_s,      spawn,          SHCMD("setxkbmap es") }, 
+	{ MODKEY,                       XK_e,      spawn,          SHCMD("setxkbmap us; kill -45 $(pidof dwmblocks)") },
+ 	{ MODKEY,                       XK_s,      spawn,          SHCMD("setxkbmap es; kill -45 $(pidof dwmblocks)") }, 
   { MODKEY,                       XK_w,      spawn,          SHCMD("$BROWSER") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
