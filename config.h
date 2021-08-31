2707606100f6ae18b889174a115e54ae2cc860e4
Fernando <fherpato8@gmail.com>
2021-08-31 10:11:20 -0600

    I added keyboard layout shortcuts

--- config.h
+++ config.h
@@ -87,6 +87,8 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_e,      spawn,          SHCMD("setxkbmap us") },
+ 	{ MODKEY,                       XK_s,      spawn,          SHCMD("setxkbmap es") }, 
   { MODKEY,                       XK_w,      spawn,          SHCMD("$BROWSER") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
